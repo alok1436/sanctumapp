a74327906919dedeeadcbfcae95a8cf930652b2c
Alok Singh <alok.singh3286@gmail.com>
2021-03-20 14:34:58 +0530

    first commit

---
+++ routes/api.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/me', function (Request $request) {
+    return $request->user();
+});
+
+Route::group(['middleware' => ['cors']], function () {
+    // ...
+    // public routes
+    Route::post('/login', 'App\Http\Controllers\Auth\ApiAuthController@login')->name('login.api');
+    Route::post('/register','App\Http\Controllers\Auth\ApiAuthController@register')->name('register.api');
+    Route::post('/logout', 'App\Http\Controllers\Auth\ApiAuthController@logout')->name('logout.api');
+    // ...
+});
+
+/*Route::post('/tokens/create', function (Request $request) {
+    $token = $request->user()->createToken($request->token_name);
+
+    return ['token' => $token->plainTextToken];
+});*/
